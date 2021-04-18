9d149ba2225dcfc909871bf09e5f0229efc1b441
root <root@La-Molecula.com>
2021-04-18 01:07:25 -0500

    first commit1

---
+++ backend.php
@@ -0,0 +1,13 @@
+<?php
+
+    $connection = mysqli_connect(
+        'localhost',
+        'LaMolecula',
+        'Rojo98_leinad0',
+        'tasks-app'
+
+    );
+
+   // if($connection) {
+    //  echo "Database is connected";
+ //   }
